8d03a4ddc66590bd8f2c8fa06a11f5d509b191ca
Darren <theinvader360@gmail.com>
2020-02-01 22:01:41 +0000

    Add simulation dir (new libgdx desktop project)

---
+++ simulation/desktop/src/com/theinvader360/sitters/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.theinvader360.sitters.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.theinvader360.sitters.Sitters;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Sitters(), config);
+	}
+}
